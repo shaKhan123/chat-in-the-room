f639a0451df757b74360bdef359f1da1347a177b
Abhishekwtv <ksha786000@gmail.com>
2020-05-12 19:28:41 +0530

    first commit

---
+++ src/components/TextContainer/TextContainer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h2>Chat with your friends <span role="img" aria-label="emoji">❤️</span></h2>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h3>People currently chatting:</h3>
+            <div className="activeContainer">
+              <h3>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h3>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
